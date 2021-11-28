77715b982a8bd7a713bb2afab64ace32f35854c5
nesma <nesmazuhier@gmail.com>
2021-11-28 09:51:27 +0200

    sender transaction tests

--- cypress/support/transaction-commands.js
+++ cypress/support/transaction-commands.js
@@ -1,29 +1,65 @@
 //Register user command
-Cypress.Commands.add("register",(firstName,lastName,username,password,confirmPassword)=>{
+Cypress.Commands.add("register", (firstName, lastName, username, password, confirmPassword) => {
     cy.visit('/signup')
-    cy.get('#firstName').type(firstName).should('have.value',firstName).and('be.focused')
-    cy.get('#lastName').type(lastName).should('have.value',lastName).and('be.focused')
-    cy.get('#username').type(username).should('have.value',username).and('be.focused')
-    cy.get('#password').type(password).should('have.value',password).and('be.focused')
-    cy.get('#confirmPassword').type(confirmPassword).should('have.value',confirmPassword).and('be.focused')
-    cy.get('span.MuiButton-label').should('contain','Sign Up').click()
-    cy.url().should('include','/signin')
+    cy.get('#firstName').type(firstName).should('have.value', firstName).and('be.focused')
+    cy.get('#lastName').type(lastName).should('have.value', lastName).and('be.focused')
+    cy.get('#username').type(username).should('have.value', username).and('be.focused')
+    cy.get('#password').type(password).should('have.value', password).and('be.focused')
+    cy.get('#confirmPassword').type(confirmPassword).should('have.value', confirmPassword).and('be.focused')
+    cy.get('span.MuiButton-label').should('contain', 'Sign Up').click()
+    cy.url().should('include', '/signin')
 });
 //Overwrite Login command
-Cypress.Commands.overwrite("login",(originalFn,username,password)=>{
-    cy.get('#username').clear().type(username).should('have.value',username).and('be.focused')
-    cy.get('#password').clear().type(password).should('have.value',password).and('be.focused')
+Cypress.Commands.overwrite("login", (originalFn, username, password) => {
+    cy.get('#username').clear().type(username).should('have.value', username).and('be.focused')
+    cy.get('#password').clear().type(password).should('have.value', password).and('be.focused')
     // cy.get('span.PrivateSwitchBase-input-33').check().should('be.checked')
-    cy.get('button[type="submit"]').should('contain','Sign In').click()
-    cy.url().should('include','')
+    cy.get('button[type="submit"]').should('contain', 'Sign In').click()
+    cy.url().should('include', '')
 })
 //Create new bank account
-Cypress.Commands.add('createBankAccount',(bankName,routingNumber,accountNumber)=>{
-    cy.get('#bankaccount-bankName-input').type(bankName).should('have.attr','placeholder','Bank Name')
-    .and('have.value',bankName).and('be.focused')
-    cy.get('#bankaccount-routingNumber-input').type(routingNumber).should('have.attr','placeholder','Routing Number')
-    .and('have.value',routingNumber).and('be.focused')
-    cy.get('#bankaccount-accountNumber-input').type(accountNumber).should('have.attr','placeholder','Account Number')
-    .and('have.value',accountNumber).and('be.focused')
-    cy.get('button[data-test="bankaccount-submit"]').should('not.be.disabled').click()
+Cypress.Commands.add('createBankAccount', (bankName, routingNumber, accountNumber) => {
+    //Verify clicking on next button displays 'create account bank' modal
+    cy.wait(5000).then(() => {
+        cy.get('body').then((elem) => {
+            if (elem.find('button[data-test="user-onboarding-next"]').length > 0) {
+                cy.get('button[data-test="user-onboarding-next"]', { timeout: 10000 }).should('contain', 'Next').click({ force: true })
+                cy.get('div[data-test="user-onboarding-dialog-title"]').should('contain', 'Create Bank Account')
+                    .and('be.visible')
+                //Verify filling bank acount feilds
+                cy.get('#bankaccount-bankName-input').type(bankName).should('have.attr', 'placeholder', 'Bank Name')
+                    .and('have.value', bankName).and('be.focused')
+                cy.get('#bankaccount-routingNumber-input').type(routingNumber).should('have.attr', 'placeholder', 'Routing Number')
+                    .and('have.value', routingNumber).and('be.focused')
+                cy.get('#bankaccount-accountNumber-input').type(accountNumber).should('have.attr', 'placeholder', 'Account Number')
+                    .and('have.value', accountNumber).and('be.focused')
+                //Verify the save button not be disabled
+                cy.get('button[data-test="bankaccount-submit"]').should('not.be.disabled')
+                    .and('have.class', 'MuiButton-containedPrimary').click()
+                //Verify the finished modal displyed after successfully creating account
+                cy.get('div[data-test="user-onboarding-dialog-title"]').should('contain', 'Finished')
+                //Verify clicking on done button
+                cy.get('button[data-test="user-onboarding-next"]', { timeout: 10000 }).should('contain', 'Done').click({ force: true })
+                //Verify the finished modal disapear after clicking done button
+                cy.get('div[data-test="user-onboarding-dialog-title"]').should('not.be.visible')
+            }
+        })
+
+    })
+
+
+})
+Cypress.Commands.add('checkAddedAccount', (bankNameSelector, bankName) => {
+    cy.get('ul > div > a:nth-child(3)').should('contain', 'Bank Accounts').click()
+    cy.url().should('include', '/bankaccounts')
+    cy.get(bankNameSelector).should('contain', bankName)
+})
+Cypress.Commands.add('checkSubmitButton', (buttonSelector) => {
+    let bankName = $('#bankaccount-bankName-input').val
+    let routingNumber = $('#bankaccount-routingNumber-input').val
+    let accountNumber = $('accountNumber').val
+
+})
+Cypress.Commands.add('logout',()=>{
+    cy.get('[data-test="sidenav-signout"]').should('contain' , 'Logout').click()
 })
--- cypress/tests/transaction-test/send_receive_test.js
+++ cypress/tests/transaction-test/send_receive_test.js
@@ -1,71 +1,163 @@
 /// <reference types="cypress" />
 
 describe("Sending and Receiving Transaction",()=>{
+    let senderUserName='NesmaSenderx3'
+    let senderPassword='12345'
+    let receiverUserName='NesmaReceiver'
+    let receiverPassword='123456'
+    let hasBankAccount=false
+    let amount=5000
+    let note="test note"
+
     before(()=>{
-        cy.visit('http://localhost:3000')
+        cy.visit(Cypress.config().baseUrl)
+    })
+    context("Creating Accounts",()=>{
+        it("Verify registering new sender user",()=>{
+            cy.register('Nesma','Sender',senderUserName,senderPassword,senderPassword)
+        })
+        it("Verify registering new Receiver user",()=>{
+            cy.register('Nesma','Receiver',receiverUserName,receiverPassword,receiverPassword)
+        })
+
     })
     context("Sending Transactions",()=>{
-        it("Verify registering new sender user",()=>{
-            cy.register('Nesma','Sender','NesmaSender','12345','12345')
-        })
+        
         it("Verify loging the registered sender user",()=>{
-            cy.login('NesmaSender','12345')
+            cy.login(senderUserName,senderPassword)
         })
-        it("Verify the user logged successfully",()=>{
-            cy.get('div:nth-child(2) > h6.MuiTypography-root.MuiTypography-subtitle2').should('contain','NesmaSender')
+        it("Verify the sender user logged successfully",()=>{
+            cy.get('div:nth-child(2) > h6.MuiTypography-root.MuiTypography-subtitle2')
+            .should('contain',senderUserName)
+            
+            // cy.request('POST','/login',{"username":senderUserName,"password":senderPassword}).its('body').should('contain',senderUserName)
         })
-        it("Verify that the 'get started' modal displayed when the user login for the first time",()=>{
-            cy.get('div[role="dialog"]').should('be.visible')
-        })
-        it("Verify clicking on next button displays 'create account bank' modal",()=>{
-            cy.get('button[data-test="user-onboarding-next"]').should('contain','Next').click()
-            cy.get('div[data-test="user-onboarding-dialog-title"]').should('contain','Create Bank Account')
-            .and('be.visible')
-        })
+        // it("Verify that the 'get started' modal displayed when the user login for the first time",()=>{
+                // cy.get('div[role="dialog"]').should('be.visible')
+
+        // cy.fixture('database.json').as('realData')
+        //     cy.get('@realData').then((data)=>{
+        //         let users=data.users
+        //         let bankaccounts=data.bankaccounts
+        //         let userId
+        //         for(var i=0; i < users.length -1; i++){
+        //             if(users[i].username==senderUserName){
+        //                 userId= users[i].id
+        //             }
+        //         }
+        //         for(var x=0; x < bankaccounts.length -1;x++){
+
+        //             if(bankaccounts[x].userId==userId){
+        //                hasBankAccount=true
+        //             }
+        //         }
+                
+        //     })
+            
+        // })
+        // it("Verify clicking on next button displays 'create account bank' modal",()=>{
+            // cy.get('button[data-test="user-onboarding-next"]').should('contain','Next').click()
+            // cy.get('div[data-test="user-onboarding-dialog-title"]').should('contain','Create Bank Account')
+            // .and('be.visible')
+        // })
         context("Create bank account",()=>{
-            before(()=>{
-                //verify the save button is disabled
-                cy.get('button[data-test="bankaccount-submit"]').should('be.disabled')
-                .and('not.have.class','MuiButton-containedPrimary')
-            })
-            it("Verify filling the bank name feild",()=>{
+    
+            it("Verify creating bank account",()=>{
                 cy.createBankAccount('Palestine','123456789','123123123')
             })
-            it("Verify the bank account added successfully",()=>{
-                cy.get('ul > div > a:nth-child(3)').should('contain','Bank Accounts').click()
-                cy.url().should('include','/bankaccounts')
-                cy.get(' ul > li > div > div:nth-child(1) > p').should('contain','Palestine')
-            })
-            after(()=>{
-                //verifty the save button enabled
-                cy.get('button[data-test="bankaccount-submit"]').should('not.be.disabled')
-                .and('have.class','MuiButton-containedPrimary')
-                cy.get('div[data-test="user-onboarding-dialog-title"]').should('contain','Finished')
-                cy.get('button[data-test="user-onboarding-next"]').should('contain','Done').click()
-                cy.get('div[data-test="user-onboarding-dialog-title"]').should('not.be.visible')
+        
+            it("Verify the account has been added successfully",()=>{
+                cy.checkAddedAccount('ul > li > div > div:nth-child(1) > p','Palestine')
+
             })
 
         })
+        
         context("Send money",()=>{
-            //verify typing insearch field
-            //verify the select contact is enabled
-            //verify the recieved user found
-            //verify click on reciver user displays the payment section
-            //verify the payment is enabled
-            //verify filling amount field
-            //verify filling note
-            //verify the button is disabled before filling fields
-            //verify the button is enabled after filling fields
-            //verify clicking on request button
-            //verify thecomplete is enabled
-            //verify the userrecived is the same
-            //verify the amout is the same
-            //verify successfully message displayed
+            //click new  transaction
+            it("Verify clicking on new button",()=>{
+                cy.get('a[href="/transaction/new"]').should('contain','New').click()
+            })
+            it("Verify the user redirect to transaction page after clicking new button",()=>{
+                cy.url().should('include','transaction/new')
+            })
+            it("Verify the 'select contact step' is enabled and other steps are disabled",()=>{
+                cy.get('.MuiStep-completed > span > span.MuiStepLabel-iconContainer > svg')
+                .should('have.class','MuiStepIcon-completed')
+                cy.get('div:nth-child(3) > span > span.MuiStepLabel-iconContainer > svg')
+                .should('not.have.class','MuiStepIcon-completed')
+                cy.get('div:nth-child(5) > span > span.MuiStepLabel-iconContainer > svg')
+                .should('not.have.class','MuiStepIcon-completed')
+            })
+            
+            it("Verify typing in search field",()=>{
+                cy.get('#user-list-search-input').type(receiverUserName)
+                .should('have.value',receiverUserName).and('have.attr','placeholder','Search...')
+            })
+            it("Verify clicking on the receiver account",()=>{
+                cy.get('.MuiPaper-elevation0.MuiPaper-rounded > ul > li:nth-child(1)').should('contain',receiverUserName).click()
+            })
+            it("Verify clicking on reciver account the 'payment' is selected",()=>{
+                cy.get('div:nth-child(3) > span > span.MuiStepLabel-iconContainer > svg')
+                .should('have.class','MuiStepIcon-active')
+            })
+            it("Verify the receiver user is displayed correctlly",()=>{
+                cy.get('div > div:nth-child(2) > h2').should('contain',receiverUserName)
+            })
+            context("Filling amount feilds",()=>{
+                beforeEach(()=>{
+                    cy.get('.MuiGrid-justify-content-xs-center > div:nth-child(1) > button')
+                    .should('be.disabled')
+                })
+                it("Verify filling amount feild",()=>{
+                    cy.get('#amount').type(amount).should('have.value',amount)
+                    .and('have.attr','placeholder','Amount')
+                })
+                it("Verify filling note feild",()=>{
+                    cy.get('#transaction-create-description-input').type(note).should('have.value',amount)
+                    .and('have.attr','placeholder','Add a note')
+                })
+                after(()=>{
+                    cy.get('.MuiGrid-justify-content-xs-center > div:nth-child(1) > button')
+                    .should('not.be.disabled')
+                })
+            })
+            context("Request payment",()=>{
+                it("Verify clicking on request button",()=>{
+                    cy.get('.MuiGrid-justify-content-xs-center > div:nth-child(1) > button')
+                    .should('contain','Request').click()
+                })
+                it("Verify the 'complete' is selected",()=>{
+                    cy.get('div:nth-child(5) > span > span.MuiStepLabel-iconContainer > svg')
+                    .should('have.class','MuiStepIcon-completed')
+                })
+                it("Verify the successfully messsage displayed",()=>{
+                    cy.get('[data-test="alert-bar-success"]').should('be.visible')
+                    .and('have.class', 'MuiAlert-filledSuccess')
+                })
+                it("Verify the payment displayed successfully ",()=>{
+                    cy.get('div.MuiBox-root.MuiBox-root-174 > div > div > h2').should('contain',amount)
+                    .and('contain',note)
+                })
+            })
 
         })
+        after(()=>{
+            cy.logout()
+        })
 
+    
+    
     })
     context("Receiving Transactions",()=>{
-        
+
+    
     })
+    
+    // Preserve cookie in every test
+    Cypress.Cookies.defaults({
+        preserve: (cookie) => {
+          return true;
+        }
+      })
 })
